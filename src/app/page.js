f3af83d7de5ac096bff8e18d15d7b94d4c93b042
Jason  Ouyang <jasonouyang@Jasons-MBP.attlocal.net>
2023-12-16 19:59:26 -0800

    add brand icons, font

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,29 @@
 import Image from "next/image";
+import { GFS_Didot } from "next/font/google";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+
+const gfs = GFS_Didot({
+  weight: "400",
+  subsets: ["greek"],
+  display: "swap",
+});
 
 export default function Home() {
   return (
-    <main className="flex">
-      <div class="bg-white border-r-1 border-indigo-500 p-6  border-[#454545] flex flex-col">
-        <h1 className="text-2xl text-center mb-5">Jason Ouyang</h1>
+    <main className={`${gfs.className} flex`}>
+      <div class="bg-white border-r  p-6  border-[#D3D3D3] flex flex-col">
+        <h1 className="text-2xl text-center mb-10 title">Jason Ouyang</h1>
         <div className="flex flex-col">
-          <div className="mb-2 border p-2 rounded-full">
-            <p className="text-xl text-center  ">Home </p>
+          <div className="mb-2 flex justify-center  p-2 ">
+            <FontAwesomeIcon icon={faGithub} style={{ width: "35" }} />
           </div>
-          <div className="mb-2 border p-2 rounded-full">
-            <p className="text-xl text-center  ">Projects </p>
+          <div className="mb-2 flex justify-center  p-2 ">
+            <FontAwesomeIcon icon={faLinkedinIn} style={{ width: "35" }} />
           </div>
-          <div className="mb-2 border p-2 rounded-full">
-            <p className="text-xl text-center  ">Contact</p>
-          </div>
-          <div className="mb-2 border p-2 rounded-full">
-            <p className="text-xl text-center  ">Articles</p>
+          <div className="mb-2 flex justify-center  p-2 ">
+            <FontAwesomeIcon icon={faEnvelope} style={{ width: "35" }} />
           </div>
         </div>
       </div>
