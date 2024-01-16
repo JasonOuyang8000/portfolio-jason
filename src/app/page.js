3b8d3bc000a69fcf75e0b3c85355dd5f2355308d
unknown <jasonouyang80@gmail.com>
2024-01-16 01:00:39 -0800

    add copy component

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { GFS_Didot } from "next/font/google";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
+
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 const gfs = GFS_Didot({
@@ -53,7 +55,20 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="bg-moonstone border-2 border-[#243c5a] rounded-md px-4 py-2 flex items-center justify-center hover:bg-[#518e9b]"></div>
+        <div className="bg-moonstone border-2 border-[#243c5a] rounded-md px-4 py-2 flex items-center justify-center hover:bg-[#518e9b]">
+          <input
+            type="text"
+            className="p-2 rounded-md inputEmail"
+            disabled
+            value={"JasonOuyang80@gmail.com"}
+          />
+
+          <FontAwesomeIcon
+            icon={faCopy}
+            style={{ width: 20, height: 20 }}
+            color="#fff"
+          />
+        </div>
       </div>
     </main>
   );
