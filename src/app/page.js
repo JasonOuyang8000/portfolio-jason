e15b269010c59deee7415a717276778f8d084e27
unknown <jasonouyang80@gmail.com>
2024-01-16 00:52:03 -0800

    fix height, fix width

--- src/app/page.js
+++ src/app/page.js
@@ -17,29 +17,43 @@ export default function Home() {
         <h1 className="text-2xl mt-6 text-center text-white mb-5 title">
           Jason Ouyang
         </h1>
-        <div className="flex justify-center">
-          <div className="mb-2 flex justify-center p-2">
-            <div className="bg-moonstone border-2 border-[#243c5a] rounded-md px-4 py-2 flex items-center justify-center hover:bg-[#518e9b]">
-              <a href="#">
-                <FontAwesomeIcon icon={faGithub} style={{ width: "35" }} />
-              </a>
-            </div>
+        <div className="flex justify-center items-center">
+          <div className=" flex justify-center p-2 mb-2">
+            <a
+              href="#"
+              className="bg-moonstone border-2 border-[#243c5a] rounded-md px-4 py-2 flex items-center justify-center hover:bg-[#518e9b]"
+            >
+              <FontAwesomeIcon
+                icon={faGithub}
+                style={{ width: 35, height: 35 }}
+              />
+            </a>
           </div>
-          <div className="mb-2 flex justify-center  p-2 ">
-            <div className="bg-moonstone border-2 border-[#243c5a] rounded-md px-4 py-2 flex items-center justify-center hover:bg-[#518e9b]">
-              <a href="#">
-                <FontAwesomeIcon icon={faLinkedinIn} style={{ width: "35" }} />
-              </a>
-            </div>
+          <div className="flex justify-center p-2 mb-2">
+            <a
+              href="#"
+              className="bg-moonstone border-2 border-[#243c5a] rounded-md px-4 py-2 flex items-center justify-center hover:bg-[#518e9b]"
+            >
+              <FontAwesomeIcon
+                icon={faLinkedinIn}
+                style={{ width: 35, height: 35 }}
+              />
+            </a>
           </div>
-          <div className="mb-2 flex justify-center  p-2 ">
-            <div className="bg-moonstone border-2 border-[#243c5a] rounded-md px-4 py-2 flex items-center justify-center hover:bg-[#518e9b]">
-              <a href="#">
-                <FontAwesomeIcon icon={faEnvelope} style={{ width: "35" }} />
-              </a>
-            </div>
+          <div className="flex justify-center p-2 mb-2">
+            <a
+              href="#"
+              className="bg-moonstone border-2 border-[#243c5a] rounded-md px-4 py-2 flex items-center justify-center hover:bg-[#518e9b]"
+            >
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                style={{ width: 35, height: 35 }}
+              />
+            </a>
           </div>
         </div>
+
+        <div className="bg-moonstone border-2 border-[#243c5a] rounded-md px-4 py-2 flex items-center justify-center hover:bg-[#518e9b]"></div>
       </div>
     </main>
   );
